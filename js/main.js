90633a6ca0cfc336118b29943bcfb7c168ce9112
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2022-10-30 15:36:32 +0500

    Change css files

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ changeBtn.addEventListener('click', function () {
     body.classList.toggle('dark');
 })
 // An event occurs when (changeBtn is clicked)
+
+// Mobile version dropdown
+
+let list = document.querySelector('.sitenav__list');
+let modeBtn = document.querySelector('.mobile__mode');
+
+modeBtn.addEventListener('click', function () {
+    list.style.display = 'block';
+})
